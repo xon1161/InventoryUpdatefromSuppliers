fc6d82a43f352c04f9c0057dcfe9104a097fc4c9
it@x-on.com.au <it@x-on.com.au>
2019-04-05 13:01:22 +0530

    Add project files.

---
+++ InventoryUpdate/ClsSqlServer.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+public class ClsSqlServer
+{
+    public string connStr;
+    private SqlConnection sqlConn;
+    private SqlTransaction sqlTran;
+
+    public void BeginTransaction()
+    {
+        this.sqlTran = this.sqlConn.BeginTransaction();
+    }
+
+    public bool close_SQLServer()
+    {
+        try
+        {
+            this.sqlConn.Close();
+            return true;
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception.ToString());
+            return false;
+        }
+    }
+
+    public void CompleteTransaction()
+    {
+        this.sqlTran.Commit();
+    }
+
+    public bool connect_SQLServer()
+    {
+        try
+        {
+            this.connStr = @"server=xoncust.database.windows.net;uid=xonuser;pwd=_xon1161;database=CustomerSite;MultipleActiveResultSets=True";
+            this.sqlConn = new SqlConnection(this.connStr);
+            this.sqlConn.Open();
+            return true;
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception.ToString());
+            return false;
+        }
+    }
+    public bool connect_SQLServer_Remote()
+    {
+        try
+        {
+            this.connStr = @"server=203.142.136.58;uid=xonuser;pwd=_xon1161;database=ClearVision;MultipleActiveResultSets=True";
+            this.sqlConn = new SqlConnection(this.connStr);
+            this.sqlConn.Open();
+            return true;
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception.ToString());
+            return false;
+        }
+    }
+    public bool execute_DML(string sql_In)
+    {
+        bool flag;
+        try
+        {
+            new SqlCommand(sql_In, this.sqlConn) { Transaction = this.sqlTran }.ExecuteNonQuery();
+            flag = true;
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine("Ex: " + exception.ToString());
+            flag = false;
+        }
+        return flag;
+    }
+
+    public DataTable execute_DT(string sql, int timeout = 0)
+    {
+        DataTable dt = new DataTable();
+        SqlCommand command;
+        SqlDataAdapter sqlAd;
+        try
+        {
+            command = new SqlCommand(sql, this.sqlConn);
+            command.CommandTimeout = timeout;
+            sqlAd = new SqlDataAdapter(command);
+            sqlAd.Fill(dt);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+            sqlAd = null;
+        }
+        sqlAd = null;
+        return dt;
+    }
+
+    public SqlDataReader execute_ReturnRecord(string sql_In,int timeout=0)
+    {
+        SqlCommand command;
+        SqlDataReader reader;
+        try
+        {
+            if (timeout > 0)
+            {
+                command = new SqlCommand(sql_In, this.sqlConn);
+                command.CommandTimeout = timeout;
+                if (this.sqlTran != null)
+                {
+                    command.Transaction = this.sqlTran;
+                }
+                reader = command.ExecuteReader();
+            }
+            else
+            {
+                command = new SqlCommand(sql_In, this.sqlConn);
+                if (this.sqlTran != null)
+                {
+                    command.Transaction = this.sqlTran;
+                }
+                reader = command.ExecuteReader();
+            }
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception.ToString());
+            reader = null;
+
+        }
+        command = null;
+        return reader;
+    }
+    public object exceuteSP_ReturnScalar(string procedureName, List<SqlParameter> parameters, int timeout = 0)
+    {
+        var data = new object();
+        using (SqlCommand sqlCommand = new SqlCommand(procedureName, sqlConn))
+        {
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+            if (timeout > 0)
+                sqlCommand.CommandTimeout = timeout;
+            if (parameters != null)
+            {
+                sqlCommand.Parameters.AddRange(parameters.ToArray());
+                data = sqlCommand.ExecuteScalar();
+            }
+        }
+        return data;
+
+    }
+
+    public DataSet exceuteSP_ReturnDataSet(string procedureName, List<SqlParameter> parameters, int timeout = 0)
+    {
+        DataSet data = new DataSet();
+        using (SqlCommand sqlCommand = new SqlCommand(procedureName, sqlConn))
+        {
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+            if (timeout > 0)
+                sqlCommand.CommandTimeout = timeout;
+            if (parameters != null)
+            {
+                sqlCommand.Parameters.AddRange(parameters.ToArray());
+                SqlDataAdapter adapter = new SqlDataAdapter(sqlCommand);
+                adapter.Fill(data);
+                adapter = null;
+            }
+        }
+        return data;
+
+    }
+}
+
---
+++ InventoryUpdate/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InventoryUpdate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (1 == 1)
+            {
+                try
+                {
+                    X: bool flag = false;
+                    //string countRows = "";
+                    int counter = 1;
+                    string ord = ConfigurationManager.AppSettings["MIIDorder"].ToString();
+                    int ik = 1;
+                    ClsSqlServer cls = new ClsSqlServer();
+                    List<string> MIIDs = new List<string>();
+                    //string count = "SELECT COUNT(distinct I.MIID) from tbl_Inventory I inner join tbl_InventorySuppliers S on I.MIID = S.MIID where MONTH(S.Date_Update)<> MONTH(I.Date_Update) and S.updaterStatus in (0, 1, 3) ";
+                    //cls.connect_SQLServer();
+                    //SqlDataReader counts=null;
+                    //try
+                    //{
+                    //    counts = cls.execute_ReturnRecord(count, 3000);
+
+                    //    while (counts != null && counts.Read())
+                    //    {
+                    //        countRows = counts["counts"].ToString();
+                    //    }
+                    //}
+                    //catch (Exception w)
+                    //{
+                    //    counts.Close();
+                    //    cls.close_SQLServer();
+                    //    goto X;
+                    //}
+                    string sql = "select MIID from tbl_Inventory where MIID in(select MIID from tbl_InventorySuppliers where updaterStatus in (0,1,3) and Availability=0 " +
+                        "except select MIID from tbl_InventorySuppliers where updaterStatus in (0,1,3) and Availability<>0) and Availability<>0";
+                    cls.close_SQLServer();
+                    cls.connect_SQLServer();
+                    SqlDataReader reader = cls.execute_ReturnRecord(sql, 1000);
+                    while (reader != null && reader.Read())
+                    {
+                        try
+                        {
+                            string MIID = reader["MIID"].ToString();
+                            MIIDs.Add(MIID);
+                            if (counter == 500)
+                            {
+                                List<SqlParameter> param = new List<SqlParameter>();
+                                param.Add(new SqlParameter("@MIIDs", string.Join(",", MIIDs)));
+                                DataSet s = new DataSet();
+                                //object data = null;
+                                //data = cls.exceuteSP_ReturnScalar("sp_Update_Inventory_Tables_Updaters", param, 600);
+                                //if(data.ToString()=="1")
+                                //    Console.WriteLine(ik + ". SCANNED 500 MIIDs");
+                                //else
+                                //    Console.WriteLine("Error occured");
+                                s = cls.exceuteSP_ReturnDataSet("sp_Update_Inventory_Tables_Updaters_debug", param, 600);
+                                MIIDs.Clear();
+                                counter = 0;
+                                if (s.Tables[5].Rows[0][0].ToString()=="1")
+                                    Console.WriteLine(ik + ". SCANNED 500 MIIDs");
+                                else
+                                    Console.WriteLine("Error occured");
+                                ik++;
+                            }
+                        }
+                        catch (Exception s)
+                        {
+                            reader.Close();
+                            flag = true;
+                            cls.close_SQLServer();
+                            counter = 0;
+                            break;
+                        }
+                        counter++;
+                    }
+                    if (flag)
+                    {
+                        goto X;
+                    }
+                    if (!reader.IsClosed)
+                        reader.Close();
+                    cls.close_SQLServer();
+                    Thread.Sleep(600000);
+                }
+                catch (Exception w)
+                { }
+            }
+        }
+    }
+}
